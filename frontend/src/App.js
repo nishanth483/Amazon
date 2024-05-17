6d7f21eff8b9c690143c72844c3a254843115c04
Nishanth Elumalai <nishanthnd63@gmail.com>
2024-05-17 12:28:27 +0530

    Home screen

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import data from './data';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import { Link } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+    <div>
+      <header>
+      <Link to="/">amazona</Link>
+      </header>
+      <main>
+      <Routes>
+      <Route path="/product/:slug" element={<ProductScreen/>}/>
+      <Route path="/" element={<HomeScreen/>}/>
+      </Routes>
+      </main>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,77 @@
+import React, { useEffect } from 'react';
+// import data from '../data';
+import { Link } from 'react-router-dom';
+import  {useState} from "react";
+import axios from 'axios';
+import { useReducer } from 'react';
+import logger from 'use-reducer-logger';
+
+const reducer = (state,action)=>{
+  switch(action.type){
+case 'FETCH_REQUEST':
+  return {...state,loading:true};
+  case 'FETCH_SUCCESS':
+    return {...state,products:action.payload,loading:false};
+case 'FETCH_FAIL':
+  return {...state,loading:false,error:action.payload};
+  default:
+    return state;
+  }
+};
+
+
+const HomeScreen = () => {
+  const[{loading,error,products},dispatch]=useReducer(logger(reducer),{
+    products:[],
+    loading:true,
+    error:'',
+  })
+  // const[products,setProducts]=useState([]);
+
+  useEffect(()=>{
+    const fetchData = async()=>{
+      dispatch({type:'FETCH_REQUEST'});
+      try{
+      const result = await axios.get('/api/products')
+      dispatch({type:'FETCH_SUCCESS',payload:result.data})
+      }catch(err)
+      {
+     dispatch({type:'FETCH_FAIL',payload:err.message})
+      }
+      // const result = await axios.get('/api/products');
+      // setProducts(result.data);
+    };
+  fetchData();
+  },[]);
+
+  return (
+    <div>
+    <h1>Featured Products</h1>
+
+    <div className="products">
+    {loading ? (
+      <div>loading...</div>
+    ) : error ? (
+      <div>{error}</div>
+    ) : (
+
+    
+      products.map(product=>(<div className="product" key={product.slug}>
+        <Link to={`/product/${product.slug}`}>
+      <img src={product.image} alt={product.name}/>
+    </Link>
+      <div className="product-info">
+      <Link to={`/product/${product.slug}`}>
+        <p>{product.name}</p>
+        </Link>
+        <p><strong>{product.price}</strong></p>
+        <button>Add to Cart</button>
+        </div>
+        </div>)))
+    }
+    </div>
+    </div>
+  );
+}
+
+export default HomeScreen;
