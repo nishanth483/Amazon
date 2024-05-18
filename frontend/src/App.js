e21523976b2701546a21061df8ff852c05bbdeaa
Nishanth Elumalai <nishanthnd63@gmail.com>
2024-05-18 11:56:06 +0530

    Rating component implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,36 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap';
 
 function App() {
 
   return (
     <BrowserRouter>
-    <div>
+    <div className='d-flex flex-column site-container'>
       <header>
-      <Link to="/">amazona</Link>
+      <Navbar bf="dark" variant="dark">
+      <Container>
+      <LinkContainer to="/">
+      <Navbar.Brand>amazona</Navbar.Brand>
+      </LinkContainer> 
+      </Container>
+      </Navbar>
+      
       </header>
       <main>
+      <Container>
       <Routes>
       <Route path="/product/:slug" element={<ProductScreen/>}/>
       <Route path="/" element={<HomeScreen/>}/>
       </Routes>
+      </Container>
       </main>
+      <footer>
+      <div className='text-center'>All rights reserved</div>
+      </footer>
     </div>
     </BrowserRouter>
   );
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom';
 import  {useState} from "react";
 import axios from 'axios';
 import { useReducer } from 'react';
-import logger from 'use-reducer-logger';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from '../components/Product';
+
+
 
 const reducer = (state,action)=>{
   switch(action.type){
@@ -21,7 +25,7 @@ case 'FETCH_FAIL':
 
 
 const HomeScreen = () => {
-  const[{loading,error,products},dispatch]=useReducer(logger(reducer),{
+  const[{loading,error,products},dispatch]=useReducer((reducer),{
     products:[],
     loading:true,
     error:'',
@@ -55,19 +59,14 @@ const HomeScreen = () => {
       <div>{error}</div>
     ) : (
 
-    
-      products.map(product=>(<div className="product" key={product.slug}>
-        <Link to={`/product/${product.slug}`}>
-      <img src={product.image} alt={product.name}/>
-    </Link>
-      <div className="product-info">
-      <Link to={`/product/${product.slug}`}>
-        <p>{product.name}</p>
-        </Link>
-        <p><strong>{product.price}</strong></p>
-        <button>Add to Cart</button>
-        </div>
-        </div>)))
+    <Row>
+      {products.map(product=>(
+        <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+       <Product product={product}></Product>
+        </Col>
+      ))}
+      </Row>
+      )
     }
     </div>
     </div>
